b5e74ce3f26c014c8b83b4ada2b5e45fbd2a6b11
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2023-07-13 18:39:40 +0300

    add API

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 // import styles from "./page.module.css";
+"use client";
+
+const API_KEY = "bb5e0d9156d1e4a8c96f5f13497bf1d5";
+const API_Read_Access_Token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYjVlMGQ5MTU2ZDFlNGE4Yzk2ZjVmMTM0OTdiZjFkNSIsInN1YiI6IjY0YjAwOTIzZTI0YjkzNWIzMGEyZjUxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.51FRsKAllK9N5ftGWYyq0JcZdZxPOTC3qRXvLJv9tVA";
 
 const getMovies = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/550?api_key=bb5e0d9156d1e4a8c96f5f13497bf1d5"
+  );
 
-  return response.json;
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
 };
 
-const Movies = () => {
+const Movies = async () => {
+  const data = await getMovies();
+  console.log("data", data);
+
   return <h1>Movies</h1>;
 };
 
